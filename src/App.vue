b50e3600422450dc17a8602c8e0283bfd35a99f2
emanuel <emanuelbotelho@ymail.com>
2020-05-30 09:59:13 -0300

    Login component created

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <login></login>
   </div>
 </template>
 
 <script>
+import Login from "./components/Login.vue";
+
 export default {
   name: 'App',
+  components: {
+    login: Login
+  },
   data() {
     return {
       msg: 'WELCOME!'
@@ -14,14 +19,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
