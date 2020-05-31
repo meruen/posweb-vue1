22618c263ee2d82587944016c32d02f466e9ec42
emanuel <emanuelbotelho@ymail.com>
2020-05-31 10:36:52 -0300

    Store added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import axios from "axios"
+import { store } from "./store"
 
 Vue.prototype.$http = axios;
 
@@ -22,5 +23,6 @@ Vue.config.productionTip = false
 new Vue({
   el: "#app",
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
